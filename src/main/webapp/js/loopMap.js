529b0844130f884eadafc63fcc85dd26a7c9d33e
krayf <krayf@northpointgis.com>
2020-12-04 17:40:58 -0500

    adding leaflet sidepanel

--- src/main/webapp/js/loopMap.js
+++ src/main/webapp/js/loopMap.js
@@ -1,5 +1,52 @@
-const mp = L.map('map').setView([15.9451, 15.468], 2);
+const mp = L.map('map').setView([38.203, -29.443], 3);
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 }).addTo(mp);
+
+// const search = new GeoSearch.GeoSearchControl({
+//     provider: new GeoSearch.OpenStreetMapProvider(),
+// });
+
+// mp.addControl(search);
+
+// const form = document.querySelector('form');
+// const input = form.querySelector('input[type="text"]');
+
+// form.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+//   const results = await provider.search({ query: input.value });
+//   console.log(results); // » [{}, {}, {}, ...]
+// });
+
+var sidebar = L.control.sidebar({
+    autopan: false,       // whether to maintain the centered map point when opening the sidebar
+    closeButton: true,    // whether t add a close button to the panes
+    container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+    position: 'right',     // left or right
+}).addTo(mp);
+
+/* add a new panel */
+var panelContent = {
+    id: 'userinfo',                     // UID, used to access the panel
+    tab: '<i class="fa fa-gear"></i>',  // content can be passed as HTML string,
+    // pane: someDomNode.innerHTML,        // DOM elements can be passed, too
+    title: 'Your Profile',              // an optional pane header
+    position: 'bottom'                  // optional vertical alignment, defaults to 'top'
+};
+sidebar.addPanel(panelContent);
+
+/* add an external link */
+sidebar.addPanel({
+    id: 'ghlink',
+    tab: '<i class="fa fa-github"></i>',
+    button: 'https://github.com/noerw/leaflet-sidebar-v2',
+});
+
+/* add a button with click listener */
+sidebar.addPanel({
+    id: 'click',
+    tab: '<i class="fa fa-info"></i>',
+    button: function (event) { console.log(event); }
+});
